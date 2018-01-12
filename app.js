bace1a9c2f60723e7d25c69a7fb35932d5d5777f
Debashish <dmajumd2@binghamton.edu>
2018-01-12 02:50:25 -0500

    Added the html and css for budgethelp app

---
+++ app.js
@@ -0,0 +1,15 @@
+var budgetController = (function() {
+	
+	var x = 20;
+	
+	var add =  function(a){
+		return x + a;
+	}
+	
+		return {
+			publicTest: function(b){
+				console.log(b);
+			}
+		}
+	
+})();
