f83b20f0e09cb64aaee3033f8f986567f5d427e7
Debashish <dmajumd2@binghamton.edu>
2018-01-12 15:14:10 -0500

    Add: Added the 3 module controller

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+//Budget Controller
 var budgetController = (function() {
 	
 	var x = 20;
@@ -8,8 +10,52 @@ var budgetController = (function() {
 	
 		return {
 			publicTest: function(b){
-				console.log(b);
+				return add(b);
 			}
 		}
 	
 })();
+
+
+//UI Controller
+var UIController = function(){
+	
+	
+	
+}();
+
+//GLobal app controller
+var controller = function(budCtrl, UICtrl){
+	
+	
+	// function for below event listner
+	var ctrlAddItem = function() {
+		
+		//1. Get the field input Data
+		
+		//2. Add the item to the budget controller
+		
+		//3. Add the item to the UI
+		
+		//4. Calculate the budget
+		
+		//5. Display the budget to UI
+		
+		console.log('Working');
+		
+	}
+	
+	// event listner for button click for tick mark
+	document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+	
+	
+	// event listner for enter key word
+	document.addEventListener('keypress', function(event){
+		
+		if(event.keyCode === 13 || event.which === 13){
+			ctrlAddItem();
+		}
+	});
+	
+	
+}(budgetController, UIController);
