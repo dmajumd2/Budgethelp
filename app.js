d9cde80118d4594f2471407083741b1ab5926de6
Debashish <dmajumd2@binghamton.edu>
2018-01-14 01:47:10 -0500

    Add: added the function for clearing input fields

--- app.js
+++ app.js
@@ -31,8 +31,6 @@ var budgetController = (function() {
 		addItem: function(type, des, val) {
 			var newItem, ID;
 			
-			
-			
 			//CREATE NEW ID
 			if(data.allItems[type].length < 0){
 			ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
@@ -41,16 +39,12 @@ var budgetController = (function() {
 			}
 			
 			
-			
-			
 			// Create new item based on 'inc' or 'exp' type
 			if(type === 'exp'){
 				newItem = new Expense(ID, des, val);
 			} else if(type === 'inc') {
 				newItem = new Income(ID, des, val);
 			}
-			
-			
 			
 			// Push it into array data structure according to the type
 			data.allItems[type].push(newItem);
@@ -94,6 +88,7 @@ var UIController = (function(){
 			},
 		
 		
+	//function addListItem
 		addListItem: function(obj, type){
 			var html, newHTML, element;
 			
@@ -117,7 +112,23 @@ var UIController = (function(){
 			
 			},
 		
-			// function to return the type, description and value to the global app controller
+		
+	//function for clearing fields
+			clearFields: function(){
+				var fields, fieldArr;
+				
+				fields = document.querySelectorAll(domStrings.inputDescription + ', ' + domStrings.inputValue);
+				
+				fieldArr = Array.prototype.slice.call(fields);
+				
+				fieldArr.forEach(function(current, index, array) {
+					current.value = "";
+				});
+				
+				fieldArr[0].focus();
+			},
+			
+	// function to return the type, description and value to the global app controller
 			getdomStrings: function(){
 				return domStrings;
 			}
@@ -150,7 +161,6 @@ var controller = (function(budCtrl, UICtrl){
 		});
 	};
 	
-	
 	// function for below event listner
 	var ctrlAddItem = function() {
 		var input, newItem;
@@ -161,13 +171,16 @@ var controller = (function(budCtrl, UICtrl){
 		newItem = budCtrl.addItem(input.type, input.description, input.value);
 		//console.log(newItem);
 		
-		
 		//3. Add the item to the UI
 		UICtrl.addListItem(newItem, input.type);
 		
-		//4. Calculate the budget
+		//4. Clear the input fields
+		UICtrl.clearFields();
 		
-		//5. Display the budget to UI
+		//5. Calculate the budget
+		
+		
+		//6. Display the budget to UI
 		
 	};
 	
