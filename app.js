a7d0069a4f908e9542a9a6f6c3573f7c6d7a2026
Debashish <dmajumd2@binghamton.edu>
2018-01-17 01:13:15 -0500

    Added the function in budget controller for deleting from the array

--- app.js
+++ app.js
@@ -83,6 +83,21 @@ var budgetController = (function() {
 			}	
 		},
 		
+		
+		deleteItem: function(type, id){
+			var ids, index;
+			
+			ids = data.allItems[type].map(function(current){
+					return current.id;
+			});
+			
+			index = ids.indexOf(id);
+			
+			if(index !== -1){
+				data.allItems[type].splice(index, 1);
+			}
+		},
+		
 		getBudget: function(){
 			return {
 				budget: data.budget,
@@ -149,7 +164,7 @@ var UIController = (function(){
 			newHTML = html.replace('%id%', obj.id);
 			newHTML = newHTML.replace('%description%', obj.description);
 			newHTML = newHTML.replace('%value%', obj.value);
-			console.log(newHTML);
+			//console.log(newHTML);
 			
 			// Insert the HTML into the DOM
 			document.querySelector(element).insertAdjacentHTML('beforeend', newHTML);	
@@ -271,9 +286,10 @@ var controller = (function(budCtrl, UICtrl){
 			//inc-1
 			splitID = itemID.split('-');
 			type = splitID[0];
-			id = splitID[1];
+			id = parseInt(splitID[1]);
 			
 			//1. delete the itme from data structure
+			budCtrl.deleteItem(type, id);
 			
 			//2. delete the item from UI
 			
